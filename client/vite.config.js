b1ac173fafaddb729a94988f91ed31b6f630c616
maher <maher@example.com>
2026-06-26 21:30:16 +0200

    fix: output client build to ../public for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: { outDir: '../public', emptyOutDir: true },
   server: {
     port: 3000,
     proxy: {
